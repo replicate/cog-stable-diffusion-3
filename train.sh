9ec9c0d59810284bd3130041b8c3b4ee435dd365
dan <dan@replicate.com>
2024-06-14 18:51:42 +0000

    qol stuff

--- train.sh
+++ train.sh
@@ -7,20 +7,20 @@ then
     pip install accelerate
 fi
 
-accelerate launch train.py \
+accelerate launch trainer.py \
   --pretrained_model_name_or_path=/src/sd3-cache \
   --instance_data_dir=/src/datasets/monstertoy \
-  --output_dir=/src/sd3-dreambooth \
+  --output_dir=/src/sd3-dreambooth-3 \
   --mixed_precision="fp16" \
-  --instance_prompt="a photo of a sks toy" \
+  --instance_prompt="a photo of a tok toy" \
   --resolution=1024 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
-  --learning_rate=1e-5 \
+  --learning_rate=1e-2 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
   --max_train_steps=1000 \
   --weighting_scheme="logit_normal" \
-  --validation_prompt="A photo of a sks toy on the beach" \
-  --validation_epochs=50 \
+  --validation_prompt="A photo of a tok toy on the beach" \
+  --validation_epochs=100 \
   --seed="0"
